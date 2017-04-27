89e7a0f1221f4fcd2c2662c8a09e5a07da7c0286
Navya Canumalla <nacanuma@microsoft.com>
2017-04-27 15:45:39 -0700

    Add ADAL libraries and require auth to view1 and view2

--- app/app.js
+++ app/app.js
@@ -3,10 +3,30 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
+  'AdalAngular',
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
 ]).
-config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+
+config(['$locationProvider', '$routeProvider', '$httpProvider','adalAuthenticationServiceProvider', function($locationProvider, $routeProvider, $httpProvider, adalProvider) {
+  $routeProvider.otherwise({redirectTo: '/'});
+
+  adalProvider.init(
+   {
+       instance: 'https://login.microsoftonline.com/',
+       tenant: 'microsoft.onmicrosoft.com',
+       clientId: '9dd0f119-edd8-4584-8981-820f67b47ea7',
+       //cacheLocation: 'localStorage', // enable this for IE, as sessionStorage does not work for localhost.
+   },
+   $httpProvider
+ );
+
+  Logging = {
+      level: 3,
+      log: function (message) {
+          console.log(message);
+      }
+  };
+ 
 }]);
